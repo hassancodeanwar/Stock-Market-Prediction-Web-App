56283fdc5bce05c91a10af5d30632f4f19481cc0
Hassan Anwar <126827134+hassancodeanwar@users.noreply.github.com>
2025-02-27 02:14:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objects as go
 
-START = "2015-01-01"
+START = "2020-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("Stock Prediction App")
